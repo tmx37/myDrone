ceec3797cbf7209c8cd73753370e2c6231e340d4
Tommaso Maruzzo <tmx37.dev@gmail.com>
2025-05-12 12:56:08 +0200

    Tested: PWR_MGMGT 1 and 2, GYRO and ACCL Config, RESETS

--- G070RB_TestPrj/Core/Src/main.c
+++ G070RB_TestPrj/Core/Src/main.c
@@ -32,24 +32,23 @@
 
 #define MPU6000_SLAVE_0 0x0068
 
-#define MPU6000_CONFIG 0x1A
-
-#define MPU6000_SELFTEST_X_ADDR 0x0D
-#define MPU6000_SELFTEST_Y_ADDR 0x0E
-#define MPU6000_SELFTEST_Z_ADDR 0x0F
-
-#define MPU6000_SELFTEST_A_ADDR 0x10
-
-#define MPU6000_WHO_AM_I 0x75
-#define MPU6000_SAMPLE_RATE_DIV 0x19
-#define MPU6000_SIGNAL_PATH_RESET 0x68
-
 #define GYRO_RESET_SIGNAL 4
 #define ACCEL_RESET_SIGNAL 2
 #define TEMP_RESET_SIGNAL 1
 
-#define MPU6000_PWE_MGMT_1 0x6B
-#define MPU6000_PWE_MGMT_2 0x6C
+#define MPU6000_SELFTEST_X_ADDR 0x0D
+#define MPU6000_SELFTEST_Y_ADDR 0x0E
+#define MPU6000_SELFTEST_Z_ADDR 0x0F
+#define MPU6000_SELFTEST_A_ADDR 0x10
+
+#define MPU6000_SAMPLE_RATE_DIV 0x19
+
+#define MPU6000_CONFIG 0x1A
+
+#define MPU6000_SMPRT_DIV 0x25
+
+#define MPU6000_GYRO_CONFIG 0x27
+#define MPU6000_ACCL_CONFIG 0x28
 
 #define MPU6000_ACC_X_OUT_H 0x3B
 #define MPU6000_ACC_X_OUT_L 0x3C
@@ -64,6 +63,13 @@
 #define MPU6000_GYRO_Y_OUT_L 0x46
 #define MPU6000_GYRO_Z_OUT_H 0x47
 #define MPU6000_GYRO_Z_OUT_L 0x48
+
+#define MPU6000_SIGNAL_PATH_RESET 0x68
+
+#define MPU6000_PWE_MGMT_1 0x6B
+#define MPU6000_PWE_MGMT_2 0x6C
+
+#define MPU6000_WHO_AM_I 0x75
 
 /* USER CODE END PTD */
 
@@ -242,16 +248,18 @@ int main(void)
     ok - Lettura last gyro
     ok - Lettura last acc
     ok - WHO_AM_I
-    - PWR_MGMT_1
-    - PWM_MGMT_2
-    - SELF_TEST
-    - GYRO_CONFIG
-    - ACCEL_CONFIG
-    - SMPRT_DIV -> SAMPLE RATE DIVIDER imposta il rate di sampling dei registri valore dei sensori, della coda fifo e di DMP
+    ok - PWR_MGMT_1
+    ok - PWM_MGMT_2
+    ok - GYRO_CONFIG
+    ok - ACCEL_CONFIG
+    // - SELF_TEST
+    ok - SMPRT_DIV -> SAMPLE RATE DIVIDER imposta il rate di sampling dei registri valore dei sensori, della coda fifo e di DMP
     ok - SIGNAL_PATH_RESET -> resetta uscite analogiche e digitali dei sensori
   */
 
-  /* TO IMPLEMENT (MPU6000 Driver)
+  /* TO IMPLEMENT (in MPU60x0 Driver)
+    - understand why I have to work with a shifted slaved device address
+
     - UtlGen_Err getDMPData(&DMPData output);
     - UtlGen_Err getGyro(&GYData output); 
     - UtlGen_Err getGyro(&ACCData output); 
@@ -273,11 +281,11 @@ int main(void)
     {
     
       #if 0
-      // WHO_AM_I
+      // ############################################################## WHO_AM_I ##############################################################
       uint8_t who_am_i_rx[1];
       HAL_I2C_Mem_Read(&hi2c1, MPU6000_SLAVE_0 << 1, MPU6000_WHO_AM_I, 1, who_am_i_rx, 1, HAL_MAX_DELAY);
 
-      // SIGNAL_PATH_RESET
+      // ############################################################## SIGNAL_PATH_RESET ##############################################################
       // gyro reset
       if (HAL_I2C_Mem_Write(&hi2c1, MPU6000_SLAVE_0 << 1, MPU6000_SIGNAL_PATH_RESET, 1, GYRO_RESET_SIGNAL, 1, HAL_MAX_DELAY) == HAL_OK) { HAL_UART_Transmit(&huart2, "Gyro reset!", strlen("Gyro reset!"), 1000); } else { HAL_UART_Transmit(&huart2, "Can't reset Gyro!", strlen("Can't reset Gyro!"), 1000); } 
       // accl reset
@@ -286,9 +294,9 @@ int main(void)
       if (HAL_I2C_Mem_Write(&hi2c1, MPU6000_SLAVE_0 << 1, MPU6000_SIGNAL_PATH_RESET, 1, TEMP_RESET_SIGNAL, 1, HAL_MAX_DELAY) == HAL_OK) { HAL_UART_Transmit(&huart2, "Temp reset!", strlen("Temp reset!"), 1000); } else { HAL_UART_Transmit(&huart2, "Can't reset Temp!", strlen("Can't reset Temp!"), 1000); }
       HAL_UART_Transmit(&huart2, "\n\r", strlen("\n\r"), 1000);
       
-      #endif
+      // ############################################################## PWR_MGMT_1 ##############################################################
 
-      // PWR_MGMT_1
+      // "CLKSEL": Clock Source select -> REMEMBER: " [...]t is highly recommended that the device be configured to use one of the gyroscopes (or an external clock source) as the clock reference for improved stability [...] " (from documentation)
       uint8_t CLKSEL_Self8MHz = 0;
       uint8_t CLKSEL_PLLXGyroRef = 1;
       uint8_t CLKSEL_PLLYGyroRef = 2;
@@ -296,8 +304,29 @@ int main(void)
       uint8_t CLKSEL_PLLEXT_36768Hz_Ref = 4;
       uint8_t CLKSEL_PLLEXT_19200KHz_Ref = 5;
       uint8_t StopClock = 7;
+      
+      // "TEMP_DIS": Disables the temperature sensor if set to "1"
+      uint8_t DisableTemperatureSentor = 8;
 
-      if (HAL_I2C_Mem_Write(&hi2c1, MPU6000_SLAVE_0 << 1, MPU6000_PWE_MGMT_1, 1, CLKSEL_Self8MHz, 1, HAL_MAX_DELAY) == HAL_OK) 
+      // "CYCLE": When "SLEEP" is disabled and this is set to "1", puts the the MPU-60x0 to sleep mode and wakes it up to di a single read at a rate determined by LP_WAKE_CTRL (defined in PWR_MGMT_2, at register 108)
+      uint8_t SetCycleOn = 32;
+
+      // "SLEEP": When set to 1, puts MPU-60x0 to sleep mode
+      uint8_t SetSleepOn = 64;
+
+      // "DEVICE_RESET": When set to 1, resets all internal registers to their default values, then this bit automatically clears to 0 once the reset is done.
+      uint8_t DeviceReset = 128;
+
+      // to read (try better)
+      uint8_t old_pwr_mgmt = 0;
+
+      // to write
+      uint8_t pwr_mgmt_reset = 0;
+      uint8_t pwr_mngmt_1_setting = CLKSEL_Self8MHz + DisableTemperatureSentor;
+
+      HAL_I2C_Mem_Read(&hi2c1, MPU6000_SLAVE_0 << 1, MPU6000_PWE_MGMT_1, 1, &old_pwr_mgmt, 1, HAL_MAX_DELAY);
+
+      if (HAL_I2C_Mem_Write(&hi2c1, MPU6000_SLAVE_0 << 1, MPU6000_PWE_MGMT_1, 1, &pwr_mngmt_1_setting, 1, HAL_MAX_DELAY) == HAL_OK) 
       { 
         HAL_UART_Transmit(&huart2, "Internal clock reference set", strlen("Internal clock reference set"), HAL_MAX_DELAY);
       } 
@@ -305,18 +334,65 @@ int main(void)
       {
         HAL_UART_Transmit(&huart2, "Cant set internal clock reference set", strlen("Cant set internal clock reference set"), HAL_MAX_DELAY);
       }
-      
-      // PWM_MGMT_2
-      
-      // SELF_TEST
-      
-      // GYRO_CONFIG
 
-      // ACCEL_CONFIG
-      
-      // SMPRT_DIV -> SAMPLE RATE DIVIDER imposta il rate di sampling dei registri valore dei sensori, della coda fifo e di DMP
+      // ############################################################## PWR_MGMT_2 ##############################################################
 
-      // GYROSCOPE_READ
+      // can be used with PWR_MGMT_1 to enable confugurations such as Accelerometer Only low power mode (CYCLE=1 + SLEEP=0 + TEMP_DIS=1 + STBY_XG,STBY_YG,STBY_ZG=1)
+
+      // "STBY_ZG": puts the Z axis gyroscope into standby mode 
+      uint8_t STBY_ZG = 0;
+      // "STBY_YG": puts the Y axis gyroscope into standby mode 
+      uint8_t STBY_ZG = 0;
+      // "STBY_XG": puts the X axis gyroscope into standby mode
+      uint8_t STBY_ZG = 0;
+      
+      // "STBY_ZA": puts the Z axis accelerometer into standby mode
+      uint8_t STBY_ZG = 0;
+      // "STBY_YA": puts the Y axis accelerometer into standby mode
+      uint8_t STBY_ZG = 0;
+      // "STBY_XA": puts the X axis accelerometer into standby mode
+      uint8_t STBY_ZG = 0;
+      
+      // "LP_WAKE_CTRL": Wake-ups frequency (Used by "CYCLE" setting of PWR_MGMT_1, register 107)
+      uint8_t LP_WAKE_CTRL_1n25Hz = 0;
+      uint8_t LP_WAKE_CTRL_5Hz = 64;
+      uint8_t LP_WAKE_CTRL_20Hz = 128;
+      uint8_t LP_WAKE_CTRL_40Hz = 192;
+
+      // to read (try better)
+      uint8_t old_pwr_mgmt = 0;
+
+      // to write
+      uint8_t pwr_mgmt_reset = 0;
+      uint8_t pwr_mngmt_1_setting = 0;
+
+      HAL_I2C_Mem_Read(&hi2c1, MPU6000_SLAVE_0 << 1, MPU6000_PWE_MGMT_1, 1, &old_pwr_mgmt, 1, HAL_MAX_DELAY);
+
+      if (HAL_I2C_Mem_Write(&hi2c1, MPU6000_SLAVE_0 << 1, MPU6000_PWE_MGMT_1, 1, &pwr_mngmt_1_setting, 1, HAL_MAX_DELAY) == HAL_OK) 
+      { 
+        HAL_UART_Transmit(&huart2, "Internal clock reference set", strlen("Internal clock reference set"), HAL_MAX_DELAY);
+      } 
+      else
+      {
+        HAL_UART_Transmit(&huart2, "Cant set internal clock reference set", strlen("Cant set internal clock reference set"), HAL_MAX_DELAY);
+      }    
+        
+      // ############################################################## SMPRT_DIV ##############################################################
+      // imposta il rate di sampling dei registri valore dei sensori, della coda fifo e di DMP
+      // sampling rate = Gyroscope output rate / (1 + SMPLRT_DIV)
+
+      uint8_t SampleRateDivider = 0;
+
+      if (HAL_I2C_Mem_Write(&hi2c1, MPU6000_SLAVE_0 << 1, MPU6000_SMPRT_DIV, 1, &SampleRateDivider, 1, HAL_MAX_DELAY) == HAL_OK)
+      {
+        HAL_UART_Transmit(&huart2, "Sample rate configured correctly!", strlen("Sample rate configured correctly!"), HAL_MAX_DELAY);
+      }
+      else
+      {
+        HAL_UART_Transmit(&huart2, "Sample rate not configured!", strlen("Sample rate not configured!"), HAL_MAX_DELAY);
+      }
+
+            // ############################################################## GYROSCOPE_READ ############################################################## 
       // 250gradi/s, 0,5 "sampling rate", measured = +40, inclinazione attuale = ?
       // variazione_gradi = +40g/s * 0,5s = +20gradi
       HAL_I2C_Master_Transmit(&hi2c1, MPU6000_SLAVE_0 << 1, &gyro_x_out_h, 1, 50);
@@ -327,18 +403,6 @@ int main(void)
 
       HAL_I2C_Master_Transmit(&hi2c1, MPU6000_SLAVE_0 << 1, &gyro_z_out_h, 1, 50);
       HAL_I2C_Master_Receive(&hi2c1, MPU6000_SLAVE_0 << 1, &gyro_z_raw[0], 2, 10);
-
-      // ACCELLEROMETER READ
-      // 1g = 9,8m/s2, 0,5s "sampling rate", mesured = 2g, m_percorsi = ?
-      // m = 2g * s2 -> 2(9.8m/s2) * (0,5s)2 = 19.6m percorso idealmente (?)
-      HAL_I2C_Master_Transmit(&hi2c1, MPU6000_SLAVE_0 << 1, &acc_x_out_h, 1, 50);
-      HAL_I2C_Master_Receive(&hi2c1, MPU6000_SLAVE_0 << 1, &acc_x_raw[0], 2, 10);
-
-      HAL_I2C_Master_Transmit(&hi2c1, MPU6000_SLAVE_0 << 1, &acc_y_out_h, 1, 50);
-      HAL_I2C_Master_Receive(&hi2c1, MPU6000_SLAVE_0 << 1, &acc_y_raw[0], 2, 10);
-
-      HAL_I2C_Master_Transmit(&hi2c1, MPU6000_SLAVE_0 << 1, &acc_z_out_h, 1, 50);
-      HAL_I2C_Master_Receive(&hi2c1, MPU6000_SLAVE_0 << 1, &acc_z_raw[0], 2, 10);
 
       // Combine high and low bytes and apply scaling
       gyro_x_final_value = (gyro_x_raw[0] << 8) + gyro_x_raw[1];
@@ -355,6 +419,18 @@ int main(void)
       gyro_y_scaled = gyro_y_final_value / 131;
       gyro_z_scaled = gyro_z_final_value / 131;
 
+      // ############################################################## ACCELLEROMETER READ ############################################################## 
+      // 1g = 9,8m/s2, 0,5s "sampling rate", mesured = 2g, m_percorsi = ?
+      // m = 2g * s2 -> 2(9.8m/s2) * (0,5s)2 = 19.6m percorso idealmente (?)
+      HAL_I2C_Master_Transmit(&hi2c1, MPU6000_SLAVE_0 << 1, &acc_x_out_h, 1, 50);
+      HAL_I2C_Master_Receive(&hi2c1, MPU6000_SLAVE_0 << 1, &acc_x_raw[0], 2, 10);
+
+      HAL_I2C_Master_Transmit(&hi2c1, MPU6000_SLAVE_0 << 1, &acc_y_out_h, 1, 50);
+      HAL_I2C_Master_Receive(&hi2c1, MPU6000_SLAVE_0 << 1, &acc_y_raw[0], 2, 10);
+
+      HAL_I2C_Master_Transmit(&hi2c1, MPU6000_SLAVE_0 << 1, &acc_z_out_h, 1, 50);
+      HAL_I2C_Master_Receive(&hi2c1, MPU6000_SLAVE_0 << 1, &acc_z_raw[0], 2, 10);
+
       // Combine high and low bytes and apply scaling
       acc_x_final_value = (acc_x_raw[0] << 8) + acc_x_raw[1];
       if (acc_x_final_value >= 32767) acc_x_final_value = acc_x_final_value - 65536;
@@ -370,7 +446,7 @@ int main(void)
       acc_y_scaled = acc_y_final_value / 16384;
       acc_z_scaled = acc_z_final_value / 16384;
 
-      // PRINT
+      // PRINT VALUES
       // Convert scaled value to string and transmit
       itoa(gyro_x_scaled, gyro_x_value_out, 10);
   	  HAL_UART_Transmit(&huart2, GY_X, strlen(GY_X), 1000);
@@ -402,6 +478,73 @@ int main(void)
       HAL_UART_Transmit(&huart2, ACC_Z, strlen(ACC_Z), 1000);
       HAL_UART_Transmit(&huart2, (uint8_t*)acc_z_value_out, strlen(acc_z_value_out), 1000);
       HAL_UART_Transmit(&huart2, new_line, strlen(new_line), 1000);
+
+    // ############################################################## GYRO_CONFIG ##############################################################
+
+      // Triggers gyroscope self-test and configures gyroscope full scale range.
+
+      // "FS_SEL": selects the full scale range of the gyroscope output
+      uint8_t FS_SEL_250 = 0;
+      uint8_t FS_SEL_500 = 8;
+      uint8_t FS_SEL_1000 = 16;
+      uint8_t FS_SEL_2000 = 24;
+
+      // "ZG_ST": activates and perform self-test on Z axis of the gyroscope
+      uint8_t ZG_SelfTest = 32;
+
+      // "YG_ST": activates and perform self-test on Y axis of the gyroscope
+      uint8_t YG_SelfTest = 64;
+
+      // "XG_ST": activates and perform self-test on X axis of the gyroscope
+      uint8_t XG_SelfTest = 128;
+
+      // Settings value to write
+      uint8_t GyroConfig_settings = FS_SEL_250;
+
+      if (HAL_I2C_Mem_Write(&hi2c1, MPU6000_SLAVE_0 << 1, MPU6000_GYRO_CONFIG, 1, &GyroConfig_settings, 1, HAL_MAX_DELAY) == HAL_OK)
+      {
+        HAL_UART_Transmit(&huart2, "Gyroscope configured correctly!", strlen("Gyroscope configured correctly!"), HAL_MAX_DELAY);
+      }
+      else
+      {
+        HAL_UART_Transmit(&huart2, "Gyroscope not configured!", strlen("Gyroscope not configured!"), HAL_MAX_DELAY);
+      }
+
+      // ############################################################## ACCEL_CONFIG ##############################################################
+    
+      // Triggers accellerometer self-test and configures gyroscope full scale range.
+
+      // "FS_SEL": selects the full scale range of the gyroscope output
+      uint8_t AFS_SEL_2g = 0;
+      uint8_t AFS_SEL_4g = 8;
+      uint8_t AFS_SEL_8g = 16;
+      uint8_t AFS_SEL_16g = 24;
+
+      // "ZA_ST": activates and perform self-test on Z axis of the accellerometer
+      uint8_t ZA_SelfTest = 32;
+
+      // "YA_ST": activates and perform self-test on Y axis of the accellerometer
+      uint8_t YA_SelfTest = 64;
+
+      // "XA_ST": activates and perform self-test on X axis of the accellerometer
+      uint8_t XA_SelfTest = 128;
+
+      // Settings value to write
+      uint8_t AccelConfig_settings = AFS_SEL_2g;
+
+      if (HAL_I2C_Mem_Write(&hi2c1, MPU6000_SLAVE_0 << 1, MPU6000_ACCL_CONFIG, 1, &AccelConfig_settings, 1, HAL_MAX_DELAY) == HAL_OK)
+      {
+        HAL_UART_Transmit(&huart2, "Accellerometer configured correctly!", strlen("Accellerometer configured correctly!"), HAL_MAX_DELAY);
+      }
+      else
+      {
+        HAL_UART_Transmit(&huart2, "Accellerometer not configured!", strlen("Accellerometer not configured!"), HAL_MAX_DELAY);
+      }
+
+      #endif
+      
+      
+
 
     }
 
