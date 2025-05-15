472670c79430418d17cb85d82d90eade86ce4d0c
Tommaso Maruzzo <tmx37.dev@gmail.com>
2025-05-15 15:42:38 +0200

    Added HMC5883L Driver structure

---
+++ G070RB_TestPrj/Drivers/DrvGY_HMC5883L/Inc/DrvGY_HMC5883L.h
@@ -0,0 +1,13 @@
+/*
+ * DrvGY_HMC5883L.h
+ *
+ *  Created on: Mar 19, 2025
+ *      Author: tmx37
+ */
+
+#ifndef DRVGY_HMC5883L_INC_DRVGY_HMC5883L_H_
+#define DRVGY_HMC5883L_INC_DRVGY_HMC5883L_H_
+
+
+
+#endif /* DRVGY_HMC5883L_INC_DRVGY_HMC5883L_H_ */
---
+++ G070RB_TestPrj/Drivers/DrvGY_HMC5883L/Src/DrvGY_HMC5883L.c
@@ -0,0 +1,8 @@
+/*
+ * DrvGY_HMC5883L.c
+ *
+ *  Created on: Mar 19, 2025
+ *      Author: tmx37
+ */
+
+
